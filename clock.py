d2692eef39b805b8a05be342349b512bb099c601
Atharva Mohite <atharvamohite20.com>
2021-05-13 02:21:02 +0530

    Probably the final commit

--- clock.py
+++ clock.py
@@ -1,13 +1,14 @@
+print("Hello World")
 from datetime import datetime
-
 from apscheduler.schedulers.blocking import BlockingScheduler
 from twi import send_alert
-def job_function():
-    print("Hello World")
 
+
+print("Hello World")
 sched = BlockingScheduler()
-
+print(sched)
+print("Hello World")
 # Schedule job_function to be called every two hours
 sched.add_job(send_alert, 'interval', seconds=10)
-
+print("Hello World")
 sched.start()
--- twi.py
+++ twi.py
@@ -16,7 +16,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36
 base = datetime.datetime.today()
 date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
 date_str = [x.strftime("%d-%m-%Y") for x in date_list]
-print(date_str)
+# print(date_str)
 
 for i in POST_CODE:
     for INP_DATE in date_str:
@@ -27,19 +27,19 @@ for i in POST_CODE:
             # print(json.dumps(resp_json, indent = 1))
             flag = False
             if resp_json["centers"]:
-                print("Available on: {}".format(INP_DATE))
+                # print("Available on: {}".format(INP_DATE))
                 text += "Available on: {}".format(INP_DATE) + "\n"
                 if(print_flag=='y' or print_flag=='Y'):
                     for center in resp_json["centers"]:
                         for session in center["sessions"]:
                             if session["min_age_limit"] <= age:
-                                print("\t", center["name"])
-                                print("\t", center["block_name"])
-                                print("\t Price: ", center["fee_type"])
-                                print("\t Available Capacity: ", session["available_capacity"])
-                                if(session["vaccine"] != ''):
-                                    print("\t Vaccine: ", session["vaccine"])
-                                print("\n\n")
+                                # print("\t", center["name"])
+                                # print("\t", center["block_name"])
+                                # print("\t Price: ", center["fee_type"])
+                                # print("\t Available Capacity: ", session["available_capacity"])
+                                # if(session["vaccine"] != ''):
+                                #     print("\t Vaccine: ", session["vaccine"])
+                                # print("\n\n")
                                 text += "Center Name " + center["name"] + "\n"
                                 text += center["block_name"] + "\n"
                                 text += "Fee:  " + center["fee_type"] + "\n"
@@ -48,18 +48,19 @@ for i in POST_CODE:
                                     text += "Vaccine Name: " + session["vaccine"] + "\n\n"
                                 
             else:
-                print("No available slots on {}".format(INP_DATE))
+                # print("No available slots on {}".format(INP_DATE))
                 text += "No available slots on {}".format(INP_DATE) + "\n"
     text += "\n"
-print(text)
+# print(text)
 
 from twilio.rest import Client 
  
 account_sid = 'ACac705688726d9eede317dec0d86f84f9' 
 auth_token = '0c73fe3177bab7665d5ec5c745a6ad11' 
 client = Client(account_sid, auth_token) 
- 
+
 def send_alert():
+    print(text)
     message = client.messages.create( 
                                 from_='whatsapp:+14155238886',  
                                 body=f'{text}',      
@@ -67,5 +68,3 @@ def send_alert():
                             ) 
     
     print(message.sid)
-
-
